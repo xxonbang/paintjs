2d3d3a7167b17dd571d981d8d85532c3ab400337
Byeongcheol Son <mackulri@gmail.com>
2019-07-25 17:22:16 +0900

    2.5강 4분 28초까지 작업본

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+const INITIAL_COLOR = "#2c2c2c";
 
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -29,8 +36,28 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+    const range = event.target.value;
+    ctx.lineWidth = range;
+}
+
+function handleModeClick() {
+    // let paint = event.srcElement.innerText;
+    // console.log(paint);
+    // paint = JSON.toString("paint");
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
 }
 
 if(canvas) {
@@ -38,4 +65,16 @@ if(canvas) {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+
+
+if(range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode) {
+    mode.addEventListener("click", handleModeClick);
 }
